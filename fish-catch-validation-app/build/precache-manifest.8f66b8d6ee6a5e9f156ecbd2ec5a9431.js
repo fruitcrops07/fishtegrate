c75984d28a503c3a6c8b525a1ac4949e695a9176
fruitcrops07 <marx.macalalag@gmail.com>
2019-10-27 10:37:44 +0800

    add beta version of fish-catch-validation-app

---
+++ fish-catch-validation-app/build/precache-manifest.8f66b8d6ee6a5e9f156ecbd2ec5a9431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10047518e7e18f9d99e64fc56aecff10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c537aa51190b48a7d9c",
+    "url": "/static/css/2.3f1cae70.chunk.css"
+  },
+  {
+    "revision": "908f30309134a70e3926",
+    "url": "/static/css/main.b628192d.chunk.css"
+  },
+  {
+    "revision": "5c537aa51190b48a7d9c",
+    "url": "/static/js/2.76a692a4.chunk.js"
+  },
+  {
+    "revision": "908f30309134a70e3926",
+    "url": "/static/js/main.84dc9bbd.chunk.js"
+  },
+  {
+    "revision": "c5822645bbcd5e3087fb",
+    "url": "/static/js/runtime-main.66c2334a.js"
+  }
+]);
